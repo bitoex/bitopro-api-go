92033444e9030e69515c83db23db9fbc64f8739d
HowardChen <HowardChen@MB-HowardChen.local>
2023-03-17 16:02:47 +0800

    Revert "Revert "chore: handle error for func ReqWithoutBody, ReqWithBody""
    This reverts commit a6287c44155ecc5eff3baf854dc4ac465bbe5749.

--- internal/req.go
+++ internal/req.go
@@ -46,7 +46,7 @@ func ReqPublic(api, proxy string) (int, string) {
 }
 
 // ReqWithoutBody func
-func ReqWithoutBody(identity, apiKey, apiSecret, method, endpoint, proxy string) (int, string) {
+func ReqWithoutBody(identity, apiKey, apiSecret, method, endpoint, proxy string) (int, string, error) {
 	payload := getNonPostPayload(identity, GetTimestamp())
 	sig := getSig(apiSecret, payload)
 	url := fmt.Sprintf("%s/%s", ApiURL, endpoint)
@@ -58,7 +58,7 @@ func ReqWithoutBody(identity, apiKey, apiSecret, method, endpoint, proxy string)
 	case "DELETE":
 		req = req.Delete(url)
 	default:
-		return http.StatusMethodNotAllowed, fmt.Sprintf("Method Not Allowed: %s", method)
+		return http.StatusMethodNotAllowed, fmt.Sprintf("Method Not Allowed: %s", method), fmt.Errorf("Method Not Allowed: %s", method)
 	}
 
 	req.Set("X-BITOPRO-APIKEY", apiKey)
@@ -66,13 +66,16 @@ func ReqWithoutBody(identity, apiKey, apiSecret, method, endpoint, proxy string)
 	req.Set("X-BITOPRO-SIGNATURE", sig)
 	req.Set("X-BITOPRO-API", "golang")
 
-	res, body, _ := req.End()
+	res, body, errList := req.End()
 
-	return res.StatusCode, body
+	if len(errList) == 0 {
+		return res.StatusCode, body, nil
+	}
+	return res.StatusCode, body, getErrByErrList(errList)
 }
 
 // ReqWithBody func
-func ReqWithBody(identity, apiKey, apiSecret, endpoint, proxy string, param map[string]interface{}) (int, string) {
+func ReqWithBody(identity, apiKey, apiSecret, endpoint, proxy string, param map[string]interface{}) (int, string, error) {
 	body, payload := getPostPayload(param)
 	sig := getSig(apiSecret, payload)
 	url := fmt.Sprintf("%s/%s", ApiURL, endpoint)
@@ -85,7 +88,19 @@ func ReqWithBody(identity, apiKey, apiSecret, endpoint, proxy string, param map[
 	req.Set("X-BITOPRO-API", "golang")
 	req.Send(body)
 
-	res, body, _ := req.End()
+	res, body, errList := req.End()
 
-	return res.StatusCode, body
+	if len(errList) == 0 {
+		return res.StatusCode, body, nil
+	}
+
+	return res.StatusCode, body, getErrByErrList(errList)
+}
+
+func getErrByErrList(errList []error) error {
+	var errStr string
+	for i, v := range errList {
+		errStr = fmt.Sprintf("%d:%s; ", i, v.Error())
+	}
+	return fmt.Errorf("errs: %s", errStr)
 }
