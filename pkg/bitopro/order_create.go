35c520ae4e85ea22574371f18bea4550dd04c1aa
vic <vic@psp-power.com.tw>
2023-05-22 21:59:04 +0800

    add batch create orders api

--- pkg/bitopro/order_create.go
+++ pkg/bitopro/order_create.go
@@ -83,3 +83,50 @@ func (api *AuthAPI) CreateOrderMarketSell(clientID int, pair, amount string) *Cr
 		"timestamp": internal.GetTimestamp(),
 	})
 }
+
+type OrderData struct {
+	Pair      string `json:"pair,omitempty"`
+	Action    string `json:"action,omitempty"`
+	Type      string `json:"type,omitempty"`
+	Price     string `json:"price,omitempty"`
+	Amount    string `json:"amount,omitempty"`
+	Timestamp int64  `json:"timestamp,omitempty"`
+}
+
+type BatchCreateOrders struct {
+	Data []struct {
+		OrderID     string `json:"orderId,omitempty"`
+		Timestamp   int64  `json:"timestamp,omitempty"`
+		Action      string `json:"action,omitempty"`
+		Amount      string `json:"amount,omitempty"`
+		IsBuy       string `json:"isBuy,omitempty"`
+		Price       string `json:"price,omitempty"`
+		TimeInForce string `json:"timeInForce,omitempty"`
+		ClientID    int    `json:"clientId,omitempty"`
+		Type        string `json:"type,omitempty"`
+	} `json:"data"`
+	StatusCode
+}
+
+// CreateOrderMarketSell Ref. https://developer.bitopro.com/docs#operation/createOrder
+func (api *AuthAPI) BatchCreateOrders(orders *OrderData) *BatchCreateOrders {
+	resp := BatchCreateOrders{}
+	data := map[string]interface{}{
+		"data": orders,
+	}
+
+	code, res, err := internal.ReqWithBody(api.identity, api.Key, api.secret, "/v3/orders/batch", api.proxy, data)
+	if err != nil {
+		resp.Error = fmt.Sprintf("req err:[%+v], res:[%+v]", err, res)
+	}
+
+	if code != 200 {
+		resp.Error = res
+	} else {
+		if err = json.Unmarshal([]byte(res), &resp); err != nil {
+			resp.Error = fmt.Sprintf("unmarshal failed, err:%+v", err)
+		}
+	}
+
+	return &resp
+}
